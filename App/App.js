349f2a025a0055c7539f18f8374c2a25cda690de
Jeevanakshay <84185011+Jeevanakshay@users.noreply.github.com>
2022-01-24 09:08:49 +0530

    Create App.js

---
+++ App/App.js
@@ -0,0 +1,17 @@
+import Sample from "./Sample";
+
+
+
+function App() {
+  return (
+    <>
+    <Sample x="1" y="2" z="2"/>
+    <Sample x="11" y="12" z="9"/>
+    <Sample x="3" y="0" z="2"/>
+    
+    </>
+  
+  );
+}
+
+export default App;
